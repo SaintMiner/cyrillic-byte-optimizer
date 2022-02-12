ed86c79fbeefbbc5d5031eb144829270600404c9
SaintMiner <dinden290@gmail.com>
2022-02-12 20:01:27 +0200

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="text-input">
+      <h2> Input </h2>
+      <textarea name="input" id="" rows="10" v-model="inputText">
+      </textarea>
+    </div>
+    <div class="text-output">
+      <h2> Output </h2>
+      <div class="processed-text">
+        {{ processedText }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
+  data() {
+    return {
+      inputText: null,
+    }
+  },
+
+  computed: {
+    processedText() {
+      return this.inputText;
+    }
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.text-input {
+  margin: 20px;
+
+  textarea {
+    width: 100%;
+  }
+}
+
+.text-output {
+  margin: 20px;  
+}
+
+.processed-text {
+  border: solid 1px gray;
+  height: 100px;
 }
 </style>
